2cdc952328109e00165b18b4be01a1dba9d4e341
Tian <tianweilow1003@gmail.com>
2025-05-28 19:57:45 +0800

    add back loader

--- src/main/java/views/FinanceViews/MakePaymentsView.java
+++ src/main/java/views/FinanceViews/MakePaymentsView.java
@@ -3,13 +3,14 @@ package views.FinanceViews;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import views.View;
 
 import java.io.IOException;
 
 public class MakePaymentsView implements View {
 
-    private HBox makePaymentsPane;
+    private StackPane makePaymentsPane;
 
     public MakePaymentsView() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/FinanceFXML/MakePayment.fxml"));
